d968e646f93734dd64ada1f58372c191ff3406ad
Thomas-Caufriez <thcaufriez@gmail.com>
2024-04-30 13:34:46 +0200

    Verif bouttons + amélioration page d'accueil

--- script/components/blockRight.js
+++ script/components/blockRight.js
@@ -13,7 +13,7 @@ const dataEcole = {
 const dataNewsletter = {
   title: `Newsletter`,
   text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-  a_href: `/pages/WIP.html`,
+  a_href: `/pages/en_developpement.html`,
   src: `/img/animatriceLisantLivreAvecEnfants.png`,
   alt: `"Animatrice lisant un livre avec des enfants"`,
 };
@@ -40,7 +40,7 @@ function generateContent_right(ids) {
                     <hr class="underline">
                     <p>${data.text}</p>
                     <article>
-                        <a href=${data.a_href}><button type="button" class="buttonCTA">Inscrivez-vous <img src="/img/play.png" alt="icone play"></button>
+                        <a href=${data.a_href}><button type="button" class="buttonInfo">En savoir plus <img src="/img/play.png" alt="icone play"></button>
                         </a>
                     </article>                    
                 </div>
